96af846835018724d96f279e712e743251e53dfd
Nephi Szvoboda <nephi.szvoboda@ti8m.ch>
2017-06-11 19:23:07 +0200

    update controller modular pattern

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 import {} from './model/reminders.model';
 import {} from './reminder-overview/reminder-overview.controller';
-import {default as FilterPanelCtrl} from './reminder-overview/filter-panel.controller';
+import {default as initFilterPanelCtrl} from './reminder-overview/filter-panel.controller';
+import {default as initMainCtrl} from './main.controller';
 
-FilterPanelCtrl.renderUI();
-
-window.FPCtrl = FilterPanelCtrl;
+window.FPCtrl = initFilterPanelCtrl();
+window.MCtrl = initMainCtrl();
