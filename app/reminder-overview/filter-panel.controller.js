c0661f6b95ba2d6e56c78c553f3585585c8f43c0
Nephi Szvoboda <nephi.szvoboda@ti8m.ch>
2017-06-11 19:22:05 +0200

    changed function to a class

--- app/reminder-overview/filter-panel.controller.js
+++ app/reminder-overview/filter-panel.controller.js
@@ -1,20 +1,31 @@
 
+class FilterPanelController {
+    constructor() {
+        this.renderUI();
+        this.sortedBy = '';
+    }
 
-function setFilterBy(filterBy) {
-    $('.filter-panel .first-button-group .btn').removeClass('btn-active');
-    $(`.filter-panel .btn[filter-option="${filterBy}"]`).addClass('btn-active');
-    console.log(`filtering by: ${filterBy}`);
+    renderUI() {
+        let template = Handlebars.templates['filter-panel'];
+        $('.filter-panel').append(template);
+    }
 
+    toggleFinished() {
+        $('.finished-filter .btn').toggleClass('btn-active');
+    }
+
+    setFilterBy(filterBy) {
+        if (this.sortedBy !== filterBy) {
+            this.sortedBy = filterBy;
+            $('.filter-panel .first-button-group .btn').removeClass('btn-active');
+            $(`.filter-panel .btn[filter-option="${filterBy}"]`).addClass('btn-active');
+            console.log(`filtering by: ${filterBy}`);
+        }
+    }
 }
 
-function renderUI() {
-    let template = Handlebars.templates['filter-panel'];
-    $('.filter-panel').append(template);
+function initFilterPanelController() {
+    return new FilterPanelController();
 }
 
-function toggleFinished() {
-    $('.finished-filter .btn').toggleClass('btn-active');
-}
-
-
-export default { setFilterBy, renderUI, toggleFinished };
+export default initFilterPanelController;
