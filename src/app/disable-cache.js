22cae5fc47491facbbae24b8a1401514af5c84a5
Nephi Szvoboda <nephi.szvoboda@ti8m.ch>
2017-06-25 17:39:30 +0200

    deleting unnecessary files

--- src/app/disable-cache.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    let scriptTag = $('<script src="dist/templates.js"></script>');
-    let scriptTag2 = $('<script src="dist/bundle.js"></script>');
-    let scripTag3 = $('<script src="app/handlebar-helpers.js"></script>');
-    let bodyEl = $('body');
-    bodyEl.append(scripTag3);
-    bodyEl.append(scriptTag);
-    bodyEl.append(scriptTag2);
-}());
